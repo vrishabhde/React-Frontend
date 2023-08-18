f4aece39de132517e1c8d34b455b4b1bdf2e2291
vrishabhde <126385928+vrishabhde@users.noreply.github.com>
2023-08-17 21:08:17 -0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <div className='abcd'>
+    <img src=""/>
+    <h1>welcome</h1>
+    <form>
+      <input type='text' placeholder='name' id='name' /><br />
+      <input type='email' placeholder='email' id='email' /><br />
+      <input type='password' placeholder='password' id='password' /><br />
+      <input type='submit' value='register' />
+    </form>
+   </div> 
+   </>
   );
 }
 
